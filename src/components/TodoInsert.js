8325e0b9ea71581f80cb54aed0079938ccdc8c4a
Kadesti <adesti3527@gmail.com>
2023-02-19 21:15:56 +0900

    todo-app complete

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,12 +2,22 @@ import { useState, useCallback } from "react";
 import { VscAdd } from "react-icons/vsc";
 import "./TodoInsert.scss";
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
   const [value, setValue] = useState("");
 
   const onChange = useCallback((e) => { setValue(e.target.value); }, []);
+
+  const onSubmit = useCallback(e => {
+    onInsert(value);
+    setValue(''); // value 값 초기화
+
+    // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+    // 이를 방지하기 위해 이 함수를 호출시킵니다.
+    e.preventDefault();
+  },[onInsert, value]);
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input placeholder="할 일을 입력하세요" value={value} onChange={onChange}/>
       <button type="submit"><VscAdd /></button>
     </form>
