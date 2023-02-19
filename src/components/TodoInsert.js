f9bb7511d3264f742b44375d627e5a8c02684434
Kadesti <adesti3527@gmail.com>
2023-02-19 14:12:52 +0900

    Todo nextID add

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,14 @@
+import { useState, useCallback } from "react";
 import { VscAdd } from "react-icons/vsc";
 import "./TodoInsert.scss";
 
 const TodoInsert = () => {
+  const [value, setValue] = useState("");
+
+  const onChange = useCallback((e) => { setValue(e.target.value); }, []);
   return (
     <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+      <input placeholder="할 일을 입력하세요" value={value} onChange={onChange}/>
       <button type="submit"><VscAdd /></button>
     </form>
   );
